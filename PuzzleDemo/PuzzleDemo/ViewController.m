f50436c4683ec2df85a15311322cf27b786d281b
qijun.kang <qijun.kang@cover.ws>
2015-12-26 17:44:45 +0800

    no message

--- PuzzleDemo/PuzzleDemo/ViewController.m
+++ PuzzleDemo/PuzzleDemo/ViewController.m
@@ -8,6 +8,9 @@
 
 #import "ViewController.h"
 
+#define WS(weakSelf)  __weak __typeof(&*self)weakSelf = self;
+
+
 @interface ViewController ()
 
 @end
@@ -17,6 +20,49 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    WS(weakSelf)
+    
+    /********************************************/
+    // 初始化view并设置背景
+    UIView *view = [UIView new];
+    view.backgroundColor = [UIColor redColor];
+    [self.view addSubview:view];
+    
+    // 使用mas_makeConstraints添加约束
+    [view mas_makeConstraints:^(MASConstraintMaker *make) {
+        // 添加大小约束（make就是要添加约束的控件view）
+        make.size.mas_equalTo(CGSizeMake(100, 100));
+        // 添加居中约束（居中方式与self相同）
+        make.center.equalTo(weakSelf.view); }];
+    
+    /********************************************/
+    UIView *blackView = [UIView new];
+    blackView.backgroundColor = [UIColor blackColor];
+    [self.view addSubview:blackView];
+    
+    // 给黑色view添加约束
+    [blackView mas_makeConstraints:^(MASConstraintMaker *make) {
+        // 添加大小约束
+        make.size.mas_equalTo(CGSizeMake(100, 100));
+        
+        // 添加左、上边距约束（左、上约束都是20）
+        make.left.and.top.mas_equalTo(20);
+    }];
+    // 初始化灰色view
+    UIView *grayView = [UIView new];
+    grayView.backgroundColor = [UIColor lightGrayColor];
+    [self.view addSubview:grayView];
+    
+    // 给灰色view添加约束
+    [grayView mas_makeConstraints:^(MASConstraintMaker *make) {
+        // 大小、上边距约束与黑色view相同
+        make.size.and.top.equalTo(blackView);
+        // 添加右边距约束（这里的间距是有方向性的，左、上边距约束为正数，右、下边距约束为负数）
+        make.right.mas_equalTo(-20);
+    }];
+    
+    /********************************************/
 }
 
 - (void)didReceiveMemoryWarning {
