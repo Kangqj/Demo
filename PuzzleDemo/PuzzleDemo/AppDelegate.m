b706b60501f055eecdf13e91ff1e945c81fb4693
Kangqijun <qijun.kang@coverme.ws>
2016-07-08 15:23:33 +0800

    local push

--- PuzzleDemo/PuzzleDemo/AppDelegate.m
+++ PuzzleDemo/PuzzleDemo/AppDelegate.m
@@ -20,6 +20,12 @@
     return YES;
 }
 
+- (void)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification {
+    //点击提示框的打开
+    application.applicationIconBadgeNumber = 10;
+}
+
+
 - (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
--- PuzzleDemo/PuzzleDemo/ViewController.m
+++ PuzzleDemo/PuzzleDemo/ViewController.m
@@ -29,6 +29,36 @@
         make.edges.equalTo(self.view).width.insets(UIEdgeInsetsMake(0, 0, 0, 0));
         
     }];
+    
+    [self addClock];
+}
+
+- (void)addClock
+{
+    [[UIApplication sharedApplication] cancelAllLocalNotifications];
+    
+    float sysVersion=[[UIDevice currentDevice]systemVersion].floatValue;
+    if (sysVersion>=8.0) {
+        UIUserNotificationType type=UIUserNotificationTypeBadge | UIUserNotificationTypeAlert | UIUserNotificationTypeSound;
+        UIUserNotificationSettings *setting=[UIUserNotificationSettings settingsForTypes:type categories:nil];
+        
+        [[UIApplication sharedApplication]registerUserNotificationSettings:setting];
+    }
+    
+    NSDate *fireDate = [[NSDate date] dateByAddingTimeInterval:10];
+    
+    UILocalNotification *notification = [[UILocalNotification alloc] init];
+    if (notification != nil) {
+        notification.fireDate = fireDate;
+        notification.timeZone = [NSTimeZone defaultTimeZone];
+        notification.soundName= UILocalNotificationDefaultSoundName;//声音，可以换成
+        //        notification.soundName = @"beep-beep.caf";
+//        notification.alertLaunchImage = [NSString stringWithFormat:@"test.png"]; //闹钟的图片。
+        notification.applicationIconBadgeNumber = 1; //闹钟的icon 数量。
+        notification.repeatInterval = kCFCalendarUnitMinute;
+        notification.alertBody = @"来消息啦！！！";
+        [[UIApplication sharedApplication] scheduleLocalNotification:notification];
+    }
 }
 
 - (void)didReceiveMemoryWarning {
