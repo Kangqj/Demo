70bc3db5fda2159710b86c5d9c4ac77572b2a79e
Kangqijun <qijun.kang@coverme.ws>
2016-11-01 17:36:26 +0800

    add GPUImage

--- GPUImageTest/GPUImageTest/ViewController.m
+++ GPUImageTest/GPUImageTest/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "GPUImage.h"
 
 @interface ViewController ()
 
---
+++ GPUImageTest/Pods/Target Support Files/Pods-GPUImageTest/Pods-GPUImageTest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GPUImageTest : NSObject
+@end
+@implementation PodsDummy_Pods_GPUImageTest
+@end
