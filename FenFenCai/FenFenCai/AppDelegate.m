f0595d51dc460865aaaff9dea79cfb5823623b68
Kangqijun <qijun.kang@coverme.ws>
2017-10-10 16:26:50 +0800

    modify config file

--- FenFenCai/FenFenCai/AppDelegate.m
+++ FenFenCai/FenFenCai/AppDelegate.m
@@ -7,6 +7,12 @@
 //
 
 #import "AppDelegate.h"
+// 引入JPush功能所需头文件
+#import "JPUSHService.h"
+// iOS10注册APNs所需头文件
+#ifdef NSFoundationVersionNumber_iOS_9_x_Max
+#import <UserNotifications/UserNotifications.h>
+#endif
 
 @interface AppDelegate ()
 
