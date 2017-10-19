ba3a18c0b34dabba5964abdafe234ac84fe58575
Kangqijun <qijun.kang@coverme.ws>
2017-10-19 11:32:53 +0800

    运行日志输出保存沙盒

---
+++ LogManage/LogManage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LogManage
+//
+//  Created by kangqijun on 2017/10/19.
+//  Copyright © 2017年 Kangqijun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LogManage/LogManage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LogManage
+//
+//  Created by kangqijun on 2017/10/19.
+//  Copyright © 2017年 Kangqijun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
